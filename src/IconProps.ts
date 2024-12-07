3ce86d0cec7eb48966e4981c3db94181b7c44b35
David Nagy <david.nagy@quinto.io>
2024-12-08 00:06:03 +0100

    [0.1.1] - hotfix hook and fix dep

--- src/IconProps.ts
+++ src/IconProps.ts
@@ -1,5 +1,3 @@
-import { useMemo } from 'react';
-
 export type IconProps = {
   width?: number;
   height?: number;
@@ -10,28 +8,26 @@ export type IconProps = {
 };
 
 export function useIconProps(props: IconProps) {
-  return useMemo(() => {
-    const { size } = props;
-    let { width, height, fill, stroke, className } = props;
-    if (!className) {
-      className = '';
-    } else {
-      className = ` ${className}`;
-    }
-    if (size !== undefined) {
-      width = size;
-      height = size;
-    }
-    if (width === undefined && height === undefined) {
-      width = 16;
-      height = 16;
-    }
-    if (fill === undefined) {
-      fill = 'currentColor';
-    }
-    if (stroke === undefined) {
-      stroke = 'currentColor';
-    }
-    return { width, height, fill, stroke, className };
-  }, [props]);
+  const { size } = props;
+  let { width, height, fill, stroke, className } = props;
+  if (!className) {
+    className = '';
+  } else {
+    className = ` ${className}`;
+  }
+  if (size !== undefined) {
+    width = size;
+    height = size;
+  }
+  if (width === undefined && height === undefined) {
+    width = 16;
+    height = 16;
+  }
+  if (fill === undefined) {
+    fill = 'currentColor';
+  }
+  if (stroke === undefined) {
+    stroke = 'currentColor';
+  }
+  return { width, height, fill, stroke, className };
 }
